7b4ad7ec0e89dee840c907931c2a14f1416a903b
Wellington Freire <wellington.freire@live.com>
2025-08-20 17:06:18 -0300

    add name

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,33 @@
+public class Name {
+    //variáveis que falam por si só
+    //A
+    public Date modifiedDate;
+    public List<Testcase> findAllTestcasesByUser(Name.User user){};
+
+    //Declarações corretas
+    //A
+    public List<Account> AccountList;
+
+    // Sem Prefixos
+    //A
+    public void deleteUser(Name.User user){};
+
+    // Evite mapa mental
+    //A
+    public URL url;
+
+    // Classe possui nomes próprios, funções devem ter verbos
+    //A
+    public class TableRepresent {};
+    public void saveCar(){};
+    //B
+
+    // Uma palavra por conceito
+    //A
+    public saveUser(){}; createAccount(){}; generateContract(){};
+
+
+    //B
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
+}
